4578df099b62e7f4232f6b4946a45fec054b86d9
abhiram <abhiramashok1062004@gmail.com>
2025-02-27 18:47:08 +0530

    SignUp

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,4 +1,4 @@
-import {House, MoveRight, ArrowRight} from "lucide-react";
+import {House, ArrowRight} from "lucide-react";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 
