7fb14092f0d1019ecb4ab92a87e3a0907615c2f7
abhiram <abhiramashok1062004@gmail.com>
2025-02-27 17:40:54 +0530

    Root

--- src/App.tsx
+++ src/App.tsx
@@ -38,16 +38,16 @@ function App() {
                 </div>
 
                 <RotatingText
-                    texts={['React', 'Bits', 'Is', 'Cool!']}
-                    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg transition-all"
+                    texts={['Get Things Done Faster at RIT!', 'Find What You Need, Instantly', 'Get Info Fast, Move Ahead!', 'Navigate RIT with Ease!']}
+                    mainClassName="px-2 sm:px-2 md:px-3 bg-[#101524] text-[#f2ddcc] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg transition-all text-[22px]"
                     staggerFrom={"last"}
                     initial={{ y: "100%" }}
                     animate={{ y: 0 }}
                     exit={{ y: "-120%" }}
-                    staggerDuration={0.025}
+                    staggerDuration={0.055}
                     splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
                     transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                    rotationInterval={2000}
+                    rotationInterval={5000}
                 />
             </div>
             <div className={'absolute bottom-12 w-[100vw] flex items-center justify-center'}>
